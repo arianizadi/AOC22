8a33cbddc33a14a2de44111dad4a340e3e87db1d
arianizadi <arianizadi@users.noreply.github.com>
2022-12-01 15:12:30 -0800

    testing readfile

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("test-set.txt")
+        .expect("File not found.");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("Error while reading file.");
+    println!("{}", data);
 }
