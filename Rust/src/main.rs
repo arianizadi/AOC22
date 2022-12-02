bd264f222297db7560c89fa2842dd6fa5188e8a2
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2022-12-01 22:54:35 -0800

    Part 2

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     let file = File::open("real-set.txt").expect("File not found!");
     let buf_reader = BufReader::new(file);
 
-    let mut total_score: u32 = 0;
+    let mut score_part1: u32 = 0;
+    let mut score_part2: u32 = 0;
 
     for line in buf_reader.lines() {
         let line = line.unwrap();
@@ -15,17 +16,32 @@ fn main() {
         player = player.trim();
 
         match (elf, player) {
-            ("A", "X") => total_score += 3 + 1,
-            ("A", "Y") => total_score += 6 + 2,
-            ("A", "Z") => total_score += 0 + 3,
-            ("B", "X") => total_score += 0 + 1,
-            ("B", "Y") => total_score += 3 + 2,
-            ("B", "Z") => total_score += 6 + 3,
-            ("C", "X") => total_score += 6 + 1,
-            ("C", "Y") => total_score += 0 + 2,
-            ("C", "Z") => total_score += 3 + 3,
+            ("A", "X") => score_part1 += 3 + 1,
+            ("A", "Y") => score_part1 += 6 + 2,
+            ("A", "Z") => score_part1 += 0 + 3,
+            ("B", "X") => score_part1 += 0 + 1,
+            ("B", "Y") => score_part1 += 3 + 2,
+            ("B", "Z") => score_part1 += 6 + 3,
+            ("C", "X") => score_part1 += 6 + 1,
+            ("C", "Y") => score_part1 += 0 + 2,
+            ("C", "Z") => score_part1 += 3 + 3,
+            _ => println!("Invalid input"),
+        }
+
+        match (elf, player) {
+            ("A", "X") => score_part2 += 0 + 3,
+            ("A", "Y") => score_part2 += 3 + 1,
+            ("A", "Z") => score_part2 += 6 + 2,
+            ("B", "X") => score_part2 += 0 + 1,
+            ("B", "Y") => score_part2 += 3 + 2,
+            ("B", "Z") => score_part2 += 6 + 3,
+            ("C", "X") => score_part2 += 0 + 2,
+            ("C", "Y") => score_part2 += 3 + 3,
+            ("C", "Z") => score_part2 += 6 + 1,
             _ => println!("Invalid input"),
         }
     }
-    println!("Total Score: {}", total_score);
+
+    println!("Total Score (Part 1): {}", score_part1);
+    println!("Total Score (Part 2): {}", score_part2);
 }
