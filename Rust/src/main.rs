bdbe6c5d0189374254d0bdc0266ff7245641788d
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2022-12-03 17:51:10 +0000

    Progress

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("test-set.txt").expect("File not found!");
+    let buf_reader = BufReader::new(file);
+
+    for line in buf_reader.lines() {
+        let (first, second)
+    }
 }
