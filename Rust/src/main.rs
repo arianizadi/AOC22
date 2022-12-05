6400c484243de26f0ea7e3bd8967ab04e761a337
arianizadi <arianizadi@users.noreply.github.com>
2022-12-05 13:46:21 -0800

    Part 2

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -7,6 +8,9 @@ fn main() {
     let buf_reader = BufReader::new(file);
 
     let mut total = 0;
+    let mut map: HashMap<char, u8> = HashMap::new();
+    let mut counter = 0;
+    let mut total_badge = 0;
 
     for line in buf_reader.lines() {
         let line = line.unwrap();
@@ -37,7 +41,49 @@ fn main() {
                 println!("Error: {}", c);
             }
         }
+
+        let mut line_chars: Vec<char> = line.chars().collect();
+        line_chars.sort();
+        line_chars.dedup();
+
+        for c in line_chars {
+            map.entry(c).and_modify(|a| *a += 1).or_insert(1);
+        }
+
+        if counter == 2 {
+            for (key, value) in &map {
+                if *value == 3 as u8 {
+                    if *key as u32 >= 97 && *key as u32 <= 122 {
+                        total_badge += *key as u32 - 96;
+                    } else if *key as u32 >= 65 && *key as u32 <= 90 {
+                        total_badge += *key as u32 - 38;
+                    } else {
+                        println!("Error: {}", key);
+                    }
+                    // println!("{} / {}", key, value);
+                    // println!("Badge: {}", total_badge);
+                }
+            }
+            map.clear();
+            counter = -1;
+        }
+        counter += 1;
     }
 
     println!("Part 1: {}", total);
+
+    // for (key, value) in &map {
+    //     if *value == 3 as u8 {
+    //         // println!("{} / {}", key, value);
+    //         if *key as u32 >= 97 && *key as u32 <= 122 {
+    //             total_badge += *key as u32 - 96;
+    //         } else if *key as u32 >= 65 && *key as u32 <= 90 {
+    //             total_badge += *key as u32 - 38;
+    //         } else {
+    //             println!("Error: {}", key);
+    //         }
+    //     }
+    // }
+
+    println!("Part 2: {}", total_badge);
 }
